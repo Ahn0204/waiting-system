efac03999c26ca5070a6b31f5e05457b8fc72de9
Ahn0204 <ahn02041@gmail.com>
2026-04-08 21:21:21 +0900

    init: project setup

---
+++ src/main/java/com/waiting/waiting_system/WaitingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.waiting.waiting_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waiting/waiting_system/WaitingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waiting.waiting_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
